7083828385004befb7cff01f2d5710ef597121ba
Axel Vera Jimenez <63276968+FlickrAV@users.noreply.github.com>
2023-06-13 10:21:09 -0400

    Multiple jumps and prototype materials

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,7 +11,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private InputAction move;
     [SerializeField] private float speed;
     [SerializeField] private float jumpHeight;
-    [SerializeField] private int amountOfJumps;
+    [SerializeField] private int amountOfJumps = 2;
+    private int currentJumps;
     [SerializeField] private float dashLenght;
     [SerializeField] private float dashCooldown;
     [SerializeField] private float gravity;
@@ -31,6 +32,11 @@ public class PlayerMovement : MonoBehaviour
     {
         ApplyGravity();
         ApplyMovement();
+
+        if(characterController.isGrounded && currentJumps != amountOfJumps)
+        {
+            currentJumps = amountOfJumps;
+        }
     }
 
     private void ApplyGravity()
@@ -52,17 +58,19 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 inputReadings = move.ReadValue<Vector2>();
         moveDirection = transform.right * inputReadings.x + transform.forward * inputReadings.y;
-        moveDirection = new Vector3(moveDirection.x, yMovement, moveDirection.z);
+        moveDirection = new Vector3(moveDirection.x, yMovement, moveDirection.z);   
         //ApplyGravity();
         characterController.Move(moveDirection * speed * Time.deltaTime);
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if(!characterController.isGrounded) return;
+        if(!characterController.isGrounded && currentJumps == 0) return;
         if(context.performed)
         {
-            yMovement += jumpHeight;
+            yMovement = jumpHeight;
+            currentJumps --;
+            Debug.Log(currentJumps);
         }
     }
 
