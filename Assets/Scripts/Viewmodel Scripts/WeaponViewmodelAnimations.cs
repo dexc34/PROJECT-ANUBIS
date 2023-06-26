649ffe3287b1266a5c24c86fc58dd526a6a54e6a
dexc34 <dexc43@gmail.com>
2023-06-25 22:30:39 -0400

    Cleaned up directory

--- Assets/Scripts/Viewmodel Scripts/WeaponViewmodelAnimations.cs
+++ Assets/Scripts/Viewmodel Scripts/WeaponViewmodelAnimations.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
-using EZCameraShake;
 
 public class WeaponViewmodelAnimations : MonoBehaviour
 {
