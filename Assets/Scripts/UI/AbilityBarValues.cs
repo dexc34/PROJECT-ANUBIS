0cbc874c9ca8ae45e95c3f184c311b189945475d
dexc34 <dexc43@gmail.com>
2023-06-29 01:53:13 -0400

    added ability cooldown bar & started work on telling players how much hp and ammo hacked enemy has

--- Assets/Scripts/UI/AbilityBarValues.cs
+++ Assets/Scripts/UI/AbilityBarValues.cs
@@ -7,9 +7,38 @@ public class AbilityBarValues : MonoBehaviour
     [Header("References")]
     SecondaryAbility abilityScript;
     [SerializeField] ProgressBar abilityBar;
+    [SerializeField] GameObject barVisuals;
 
+    [Header("Colors")]
+    [SerializeField] Color barFullColor;
+    [SerializeField] Color barRechargingColor;
+
+    float timeElapsed;
     private void Start()
     {
         abilityScript = GameObject.Find("Player").GetComponent<SecondaryAbility>();
+        abilityBar.maximum = abilityScript.abilityCooldown;
+        abilityBar.current = abilityScript.abilityCooldown;
+    }
+
+    private void Update()
+    {
+        if (!abilityScript.canUseAbility)
+            UpdateBar();
+        if (abilityScript.canUseAbility)
+            ResetTimer();
+    }
+
+    void UpdateBar()
+    {
+        abilityBar.color = barRechargingColor;
+        abilityBar.maximum = abilityScript.abilityCooldown;
+        timeElapsed += Time.deltaTime;
+        abilityBar.current = timeElapsed;
+    }
+    void ResetTimer()
+    {
+        abilityBar.color = barFullColor;
+        timeElapsed = 0;
     }
 }
